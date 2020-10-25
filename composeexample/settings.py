06bc773e7e21eaeeb74d653509c64a9b874152ec
naoto-ysd <na_oto1111@yahoo.co.jp>
2020-10-25 23:48:48 +0900

    create gitignorte

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'iolg@wapb(x3t0^iqc(wf4p+co#ms%932906-%%vzwk&9-r(+6'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
